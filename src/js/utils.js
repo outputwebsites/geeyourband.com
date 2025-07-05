a3655e18892a735cc7c25e281d1e58cdaeb3533d
derek-perry <dp@derek-perry.com>
2025-07-05 02:05:54 -0400

    Fix Missing Show Data Fallbacks
    Fixed fallback data for missing show data in the JSON-LD generation. Added Google Maps link to a show's location.

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,16 +1,3 @@
-export function formatDate(date) {
-  return new Date(date).toLocaleDateString("en-US", {
-    timeZone: "UTC",
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-};
-
-export function getCurrentYear() {
-  return new Date().getFullYear();
-}
-
 import { getImage } from "astro:assets";
 export async function getOptimizedImage(image) {
   const optimizedImage = await getImage({
